e4f822dd938eb661c50fb2e0f5d30b1c136bfe8c
Leonardo de Paula <leodepaula315@gmail.com>
2025-07-28 12:33:57 -0300

    first commit

---
+++ src/test/java/io/github/leonarddepaula/libaryapi/LibaryapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.leonarddepaula.libaryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibaryapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
