35fdf731022c2a3e629097fd72205444d3bf125c
Leonardo de Paula <leodepaula315@gmail.com>
2025-09-02 11:10:18 -0300

    search filter author

--- src/main/java/io/github/leonarddepaula/libaryapi/service/AutorService.java
+++ src/main/java/io/github/leonarddepaula/libaryapi/service/AutorService.java
@@ -4,6 +4,7 @@ import io.github.leonarddepaula.libaryapi.model.Autor;
 import io.github.leonarddepaula.libaryapi.repository.AutorRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -27,5 +28,21 @@ public class AutorService {
 
     public void deletar(Autor autor) {
         repository.delete(autor);
+    }
+
+    public List<Autor> pesquisar(String nome, String nacionalidade) {
+        if (nome != null && nacionalidade != null) {
+            return repository.findByNomeAndNacionalidade(nome, nacionalidade);
+        }
+
+        if (nome != null) {
+            return repository.findByNome(nome);
+        }
+
+        if (nacionalidade != null) {
+            return repository.findByNacionalidade(nacionalidade);
+        }
+
+        return repository.findAll();
     }
 }
